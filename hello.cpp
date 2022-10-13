ff4124cde8f41f4deb630b661649f19a54124c68
1kimli2 <109332286+1kimli2@users.noreply.github.com>
2022-10-13 17:07:34 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include"windows.h"
+int main()
+{
+    int a=1;
+    cout << "a=" << a <<endl;
+    cout << " hello ,world" <<endl;
+    cout << " nihao " <<endl;
+    system("pause");
+    return 0;
+}
